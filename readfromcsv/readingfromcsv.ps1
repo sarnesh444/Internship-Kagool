274affe84b023781bb87ae7545b7e1eb2a300e3c
Saranesh Kanumuri <46394329+sarnesh444@users.noreply.github.com>
2020-06-06 21:36:26 +0530

    Add files via upload

---
+++ readfromcsv/readingfromcsv.ps1
@@ -0,0 +1,19 @@
+﻿$pathe=read-host -prompt "Enter path of your revenue csv"
+if(Test-Path -Path $pathe)
+{
+    Write-Output "valid path"
+    $output_path="C:\Users\saran\Desktop\kagool\task2powershell\output.txt"
+    $P = Import-Csv -Path $pathe
+    $P | Format-Table
+    $check=$P | ForEach-Object {
+    if($($_.dept_rev -gt 2500)){
+    Write-Host "$($_.dept_name), is the dept which has revenue greater than 2500"
+    }
+    } 
+    $check | Out-File -FilePath $output_path
+}
+else
+{
+    Write-Output "invalid path"
+}
+# C:\Users\saran\Desktop\kagool\task2powershell\sample.csv
